286a91d3b7f25d93978a88d793e85d4bebaa98b8
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-10-13 00:09:26 +0700

    membuat fungsi withNamedArgument, withDefaultParameter, withReturn dan main function

--- app/src/main/java/com/example/activity1_0130/Functions.kt
+++ app/src/main/java/com/example/activity1_0130/Functions.kt
@@ -2,11 +2,38 @@ package com.example.activity1_0130
 
 fun withoutParameter() {
     println("== withoutParameter ==")
-    println("Hello, PAM!")
+    println("Hello, World!")
 }
 
 fun withParameter(name: String) {
     println()
     println("== withParameter ==")
     println("Hello, $name!")
+}
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name ! You are $age years old.")
+}
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! you are $age years old. ")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+
+fun main () {
+    withoutParameter()
+    withParameter("john")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
 }
