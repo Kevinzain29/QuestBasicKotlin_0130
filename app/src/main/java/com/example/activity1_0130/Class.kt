5e6c3b2a0e0517e93f78f5a0a8834ca60e5f6617
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-10-12 23:30:01 +0700

    membuat main function

--- app/src/main/java/com/example/activity1_0130/Class.kt
+++ app/src/main/java/com/example/activity1_0130/Class.kt
@@ -3,3 +3,13 @@ package com.example.activity1_0130
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.email)
+
+    contact.email = "jane@gmail.com"
+
+    println(contact.email)
+}
