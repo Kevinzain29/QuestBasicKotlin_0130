27df383f3e37d6970b3c03b57b2af4b2b390e51a
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-10-13 00:04:19 +0700

    membuat fungsi dengan parameter dan tanpa parameter

---
+++ app/src/main/java/com/example/activity1_0130/Functions.kt
@@ -0,0 +1,12 @@
+package com.example.activity1_0130
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, PAM!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
