7776ac321393d177fc082781efc5ece8ae030ce4
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-10-13 00:29:26 +0700

    tipe data primitif

---
+++ app/src/main/java/com/example/activity1_0130/TipeData.kt
@@ -0,0 +1,34 @@
+package com.example.activity1_0130
+
+fun main() {
+    // Byte
+    val ByteValue: Byte = 10
+    println("Byte: $ByteValue")
+
+    //int
+    val intValue: Int = 30
+    println("int: $intValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("float: $floatValue")
+
+    // Double
+    val DoubleValue: Double = 60.6
+    println("Double: $DoubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("char: $charValue")
+
+    // Boolean
+    val BooleanValue: Boolean = true
+    println("Boolean: $BooleanValue")
+
+    // string
+    var stringValue: String = "hello, word!"
+    println("string: $stringValue")
+
+    stringValue = "hello, kotlin!"
+    println("string: $stringValue")
+}
